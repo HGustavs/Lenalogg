25b131091c649b0cf7fd938f5be8f9b773861737
HGustavs <henrik.gustavsson@his.se>
2016-09-13 20:59:51 +0200

    Basic Diagram and Day Filtering

--- loglena.php
+++ loglena.php
@@ -5,31 +5,73 @@
 
    <script>
 		var ctx;
+		
 		function initcanvas()
 		{
-				str="<table>";
-				for(i=0;i<entries.length;i++){
-						str+="<tr>";
-						str+="<td>"+entries[i].id+"</td>";
-						str+="<td>"+entries[i].timest+"</td>";
-						str+="<td>"+entries[i].service+"</td>";
-						str+="<td>"+entries[i].interval+"</td>";						
-						str+="</tr>";
-				}
-				str+="</table>";
-				
-				
-				
-				document.getElementById("content").innerHTML=str;
-				
-/*
+				// Setup canvas
 				var canvas = document.getElementById("myCanvas");
 				if (canvas.getContext) {
 				    ctx = canvas.getContext("2d");
-				    ctx.strokeRect(0,0,900,500);
-				
+				    ctx.strokeStyle="#000";
+						ctx.beginPath();
+				    ctx.moveTo(0,550);
+				    ctx.lineTo(1500,550);
+				    ctx.stroke();
+				    				
 				}
-*/
+
+				// Sort on interval length
+				entries.sort(
+						function(a, b){
+								return a.interval-b.interval
+						}
+				);
+				
+				var filter="UNKO";
+				
+				str="<table>";
+				for(i=0;i<entries.length;i++){
+						var ar=entries[i].timest.substring(0,4);
+						var man=entries[i].timest.substring(5,7);
+						var dag=entries[i].timest.substring(8,10);
+						var tim=entries[i].timest.substring(11,13);
+						var min=entries[i].timest.substring(14,16);						
+						var sec=entries[i].timest.substring(17,19);
+						var interval=entries[i].interval;
+						var xk=parseInt(min)+(parseInt(tim)*60.0);
+						var ln=interval*0.1;
+						
+						if((ar=="2016" && man=="09"&&dag=="09") || filter=="UNK"){
+								str+="<tr>";
+								str+="<td>"+entries[i].id+"</td>";
+								str+="<td>"+entries[i].timest+"</td>";
+								str+="<td>"+entries[i].service+"</td>";
+								str+="<td>"+entries[i].interval+"</td>";
+
+								str+="<td>"+tim+"</td>";
+								str+="<td>"+min+"</td>";
+								str+="<td>"+sec+"</td>";
+								str+="<td>"+xk+"</td>";
+
+								str+="</tr>";
+								
+								var sst="#4b4";
+								if(entries[i].service=="courseedservice.php") sst="#b44";
+								if(entries[i].service=="resultedservice.php") sst="#44b";								
+								
+								
+								
+								ctx.strokeStyle=sst;								
+								ctx.beginPath();
+								ctx.moveTo(xk,550);
+								ctx.lineTo(xk,550-ln);
+								ctx.stroke();
+						}						
+
+				}
+				str+="</table>";
+				
+				document.getElementById("content").innerHTML=str;
 		} 
     
 <?php
@@ -37,7 +79,7 @@
 		date_default_timezone_set("Europe/Stockholm");
 		
 		$pdo = new PDO('sqlite:loglena4.db');
-		$sql = "SELECT id,uuid,service,datetime(timestamp/1000, 'unixepoch', 'localtime') as timest,timestamp as timez FROM serviceLogEntries LIMIT 4000";
+		$sql = "SELECT id,uuid,service,datetime(timestamp/1000, 'unixepoch', 'localtime') as timest,timestamp as timez FROM serviceLogEntries LIMIT 30000 OFFSET 90000";
 		$stmt = $pdo->prepare($sql);
 		$stmt->execute();
 		
@@ -53,6 +95,8 @@
 								$datetime1 = $row['timez'];
 								$datetime2 = $oldrow['timez'];
 								$interval = $datetime1-$datetime2;
+								
+								if($oldrow['uuid']!=$row['uuid']) $interval=-1;
 
 								echo '"id":"'.$row['id'].'",';
 								echo '"uuid":"'.$row['uuid'].'",';
@@ -72,8 +116,12 @@
 </head>
 <body onload="initcanvas();">
 
-				<div id="content">Foo!</div>
+	      <canvas id="myCanvas" width="2200" height="600" style="display:block;border:1px solid red;">
+	      </canvas>
 
-	      <canvas id="myCanvas" width="600" height="600" style="display:none;">
+				<div id="content" style="border:1px solid green;" >
+					Foo!
+				</div>
+
 </body>
 </html>
