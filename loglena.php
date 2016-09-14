c16c4e82418e154e5ffa9ecd7fb63be90a069712
HGustavs <henrik.gustavsson@his.se>
2016-09-14 10:10:20 +0200

    View option and additional dynamic filter

--- loglena.php
+++ loglena.php
@@ -11,6 +11,8 @@
 				var yearz=document.getElementById("yearz").value;
 				var monthz=document.getElementById("monthz").value;
 				var dayz=document.getElementById("dayz").value;
+				var viewz=document.getElementById("viewz").value;
+				var servz=document.getElementById("servz").value;
 				
 				ctx.clearRect(0,0,1500,752);
 				
@@ -47,7 +49,6 @@
 				    ctx.lineTo(i*60,555);
 				    ctx.stroke();
 		    }
-		    				
 
 				// Sort on interval length
 				entries.sort(
@@ -59,6 +60,10 @@
 				var filter="UNKO";
 				
 				str="<table>";
+
+				// Filter Service List
+				var olist=[];
+
 				var j=0;
 				for(i=0;i<entries.length;i++){
 						var ar=entries[i].timest.substring(0,4);
@@ -72,6 +77,11 @@
 						var ln=interval*0.1;
 						
 						if(( ar==yearz && man==monthz && dag==dayz) || filter=="UNK"){
+						
+								if(olist.indexOf(entries[i].service)==-1){
+										olist.push(entries[i].service);
+								}
+								
 								str+="<tr>";
 								str+="<td>"+entries[i].id+"</td>";
 								str+="<td>"+entries[i].timest+"</td>";
@@ -110,8 +120,22 @@
 
 				}
 				str+="</table>";
-				
 				document.getElementById("content").innerHTML=str;		
+				
+				str="";
+				if(servz=="ALL"){
+						str+="<option selected='selected'>ALL</option>";
+				}else{
+						str+="<option>ALL</option>";
+				}
+				for(i=0;i<olist.length;i++){
+						if(servz==olist[i]){
+								str+="<option selected='selected'>"+olist[i]+"</option>";
+						}else{
+								str+="<option>"+olist[i]+"</option>";
+						}
+				}
+				document.getElementById("servz").innerHTML=str;								
 		
 		}
 		
@@ -175,6 +199,8 @@
 								<td>Year:&nbsp;<select onchange="redraw()" id="yearz"><option>2015</option><option>2016</option><option>2017</option></select></td>
 								<td>Month:&nbsp;<select onchange="redraw()" id="monthz"><option value="01">Jan</option><option value="02">Feb</option><option value="03">Mar</option><option value="04">Apr</option><option value="05">May</option><option value="06">Jun</option><option value="07">Jul</option><option value="08">Aug</option><option value="09">Sep</option><option value="10">Oct</option><option value="11">Nov</option><option value="01">Dec</option></select></td>
 								<td>Day:&nbsp;<select onchange="redraw()" id="dayz"><option>01</option><option>02</option><option>03</option><option>04</option><option>05</option><option>06</option><option>07</option><option>08</option><option>09</option><option>10</option><option>11</option><option>12</option><option>13</option><option>14</option><option>15</option><option>16</option><option>17</option><option>18</option><option>19</option><option>20</option><option>21</option><option>22</option><option>23</option><option>24</option><option>25</option><option>26</option><option>27</option><option>28</option><option>29</option><option>30</option><option>31</option></select></td>
+								<td>View:&nbsp;<select onchange="redraw()" id="viewz"><option>Daily R</option><option>View R</option></select></td>
+								<td>Service:&nbsp;<select onchange="redraw()" id="servz"></select></td>
 						</table>
 				</div>
 	      
